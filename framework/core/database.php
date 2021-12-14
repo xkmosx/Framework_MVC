6c8382908ebec32e750778ba7aa04ec4200c3ede
Carlos Andrés Mosquera García <90432643+xkmosx@users.noreply.github.com>
2021-12-14 14:52:12 -0500

    Delete framework directory

--- framework/core/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Database {
-	#---------------------------------------------
-	public static function conectar() {
-		$conexion =  new PDO("mysql:charset=".CHARSET.";host=".HOST.";dbname=".DB."",USER,PASSWORD);
-		return $conexion;
-	}
-}
--- framework/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require_once "settings/config.php";
-require_once "core/database.php";
-require_once "core/loader.php";
-require_once "core/router.php";
